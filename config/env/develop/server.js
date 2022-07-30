ced9172c78c31cab562dcfb5baba2b48b14ebebe
edcalderon <edwardca12@hotmail.com>
2022-07-30 17:24:56 -0500

    middleware cors

--- config/env/develop/server.js
+++ config/env/develop/server.js
@@ -2,6 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: 'http://localhost:1337',
+  "proxy": {
+    "enabled": true,
+    "ssl": true,
+    "host": 'http://localhost:3000',
+    "port": 443
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
---
+++ config/env/production/middlewares.js
@@ -0,0 +1,9 @@
+module.exports =  ({env}) => ({
+  settings: {
+      cors: {
+          enabled: true,
+          headers: '*',
+          origin: ['*'],
+      },
+  },
+});
