9d0fe3bc13b2b85f15bbb49b208eda1151c99ba7
AugustoAleGon <aagg83556@hotmail.com>
2022-07-30 20:12:26 -0500

    :zap: enable cloudinary

--- config/env/develop/plugins.js
+++ config/env/develop/plugins.js
@@ -9,4 +9,16 @@ module.exports = ({ env }) => ({
         defaultReplyTo: 'contacto@bancannapps.co',
       },
     },
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
 });
--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -10,4 +10,16 @@ module.exports = ({ env }) => ({
         testAddress: '',
       },
     },
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
 });
